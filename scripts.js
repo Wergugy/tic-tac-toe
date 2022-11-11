396a13e991b8a9de0e2c31b9c6b7d101ce44b754
Wergugy <wergugy@gmail.com>
2022-11-10 20:29:19 -0800

    Winner check resolved
    Correctly checks for a winner and logs it.
    also checks for a tie and logs it. Will now
    add some UI for adding a name and choosing a
    mark. And of course starting a game with a
    button.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
 const game = (() => {
-        let _turn = 0;
+        let _turn = 0
+        ,_player1
+        ,_player2;
+
+        const _checkWinner = () => {
+                if (_turn === 9) console.log('Its a Tie!');
+                else if (gameBoard.checkTiles(_player1['mark'])) {
+                        console.log(`${_player1['name']} Wins!`);
+                        _end();
+                }
+                else if (gameBoard.checkTiles(_player2['mark'])) {
+                        console.log(`${_player2['name']} Wins!`);
+                        _end();
+                }; 
+        };
+
+        const _makePlayers = () => {
+                _player1 = player('Jimothy', 'X')
+                _player2 = player('Hestophar', 'O');
+        };
+
+        const _end = () => {
+                gameBoard.lock();
+        }
 
         const currentMark = () => {
                 if ((_turn % 2) === 0) return 'X';
@@ -8,11 +31,12 @@ const game = (() => {
 
         const nextTurn = () => {
                 _turn++;
-                if (_turn === 9);
+                _checkWinner();
         };
 
         const start = () => {
                 _turn = 0;
+                _makePlayers();
                 gameBoard.reset();
                 gameBoard.setUp();
         };
@@ -21,7 +45,7 @@ const game = (() => {
 })();
 
 const gameBoard = (() => {
-        const _tiles = document.querySelectorAll('td.place');
+        const _tiles = Array.from(document.querySelectorAll('td.place'));
  
         const _placeMark = (e) => {
                 e.preventDefault();
@@ -37,11 +61,53 @@ const gameBoard = (() => {
                 _tiles.forEach(t => t.textContent = null);
         };
 
-        return {setUp,reset};
+        const lock = () => {
+                _tiles.forEach(t => t.removeEventListener('click', _placeMark, {once:true}));
+        }
+
+        const checkTiles = (mark) => {
+                switch (true) { 
+                        case _tiles.filter(t => t.classList.contains('r1'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        case _tiles.filter(t => t.classList.contains('r2'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        case _tiles.filter(t => t.classList.contains('r3'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        case _tiles.filter(t => t.classList.contains('c1'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        case _tiles.filter(t => t.classList.contains('c2'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        case _tiles.filter(t => t.classList.contains('c3'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        case _tiles.filter(t => t.classList.contains('d1'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        case _tiles.filter(t => t.classList.contains('d2'))
+                                .every(t => t.textContent === mark):
+                                        mark = true;
+                                        break;
+                        default:
+                                mark = false;
+                };
+                return mark;
+        };
+
+        return {setUp,reset, checkTiles, lock};
 })();
 
 const player = (name, mark) => {
-      
-
         return {name, mark};
 };
